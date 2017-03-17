932a1a7bc163284a1f0ee05e3fd48c803f4ab9cc
renato <renato@twobo.com>
2017-03-17 20:33:04 +0100

    Rewrote sample.

--- src/test/kotlin/com/athaydes/samples/sample.kt
+++ src/test/kotlin/com/athaydes/samples/sample.kt
@@ -1,78 +1,48 @@
 package com.athaydes.samples
 
-import com.athaydes.kunion.Union
 
 /**
  * This is a sample usage of the KUnion library.
  */
+import com.athaydes.kunion.Union
+import java.util.Collections
 
-/*
-// suites
-class Spades() {}
-class Diamonds() {}
-class Hearts() {}
-class Clubs() {}
-
-// ranks
-class Ace() {}
-class Jack() {}
-class Queen() {}
-class King() {}
-
-alias Rank => Integer|Ace|Jack|Queen|King;
-alias Suit => Spades|Diamonds|Hearts|Clubs;
-
- */
-
-
-sealed class Suit
+enum class Suit
 {
-    object Spades : Suit()
-    object Diamonds : Suit()
-    object Hearts : Suit()
-    object Clubs : Suit()
+    Spades, Diamonds, Hearts, Clubs
 }
 
-object Ace
-object Jack
-object Queen
-object King
-
-
-/**
- * Rank is a union because we couldn't make a sealed class containing an integer
- */
-typealias Rank = Union.U2<Int, Union.U4<Ace, Jack, Queen, King>>
-
-typealias IntRank = Union.U2.U2_1<Int>
-typealias NonIntRank = Union.U2.U2_2<Union.U4<Ace, Jack, Queen, King>>
-
-
-typealias UAce = Union.U4.U4_1<Ace>
-typealias UJack = Union.U4.U4_2<Jack>
-typealias UQueen = Union.U4.U4_3<Queen>
-typealias UKing = Union.U4.U4_4<King>
-
-fun numericValueOf(rank: Rank): Int = when (rank)
+enum class Symbol
 {
-    is IntRank    -> rank.value
-    is NonIntRank -> when (rank.value)
-    {
-        is UAce   -> 14
-        is UJack  -> 11
-        is UQueen -> 12
-        is UKing  -> 13
-    }
+    Ace, Jack, Queen, King
 }
+
+typealias Rank = Union.U2<Int, Symbol>
+
+fun rank(number: Int): Rank = Union.U2.ofA(number)
+
+fun rank(symbol: Symbol): Rank = Union.U2.ofB(symbol)
+
+
+fun numericValueOf(rank: Rank): Int = rank.use(
+        { number -> number },
+        { rank ->
+            when (rank)
+            {
+                Symbol.Ace   -> 14
+                Symbol.Jack  -> 11
+                Symbol.Queen -> 12
+                Symbol.King  -> 13
+            }
+        })
 
 fun numericValueOf(suit: Suit): Int = when (suit)
 {
-    is Suit.Spades   -> 10
-    is Suit.Diamonds -> 9
-    is Suit.Hearts   -> 8
-    is Suit.Clubs    -> 7
+    Suit.Spades   -> 10
+    Suit.Diamonds -> 9
+    Suit.Hearts   -> 8
+    Suit.Clubs    -> 7
 }
-
 
 data class Card(val rank: Rank, val suit: Suit) : Comparable<Card>
 {
@@ -88,18 +58,20 @@ data class Card(val rank: Rank, val suit: Suit) : Comparable<Card>
 
 }
 
-// TODO
 fun main(args: Array<String>)
 {
-    val cards = (1..10).map {
-        val rand = Math.ceil(Math.random() * 15).toInt()
-        val rank: Rank = when
-        {
-            rand < 11 -> IntRank(rand)
-            rand < 12 -> NonIntRank(UJack(Jack))
-            rand < 13 -> NonIntRank(UQueen(Queen))
-            rand < 14 -> NonIntRank(UKing(King))
-            else      -> NonIntRank(UAce(Ace))
-        }
-    }
+    val ranks = (1..10).map(::rank) +
+            listOf(Symbol.Ace, Symbol.Jack, Symbol.Queen, Symbol.King).map(::rank)
+
+    val suites = listOf(Suit.Clubs, Suit.Hearts, Suit.Diamonds, Suit.Spades)
+
+    val cards = ranks.flatMap { r -> suites.map { s -> Card(r, s) } }.toMutableList()
+
+    println("Sorted cards:")
+    println(cards)
+
+    Collections.shuffle(cards)
+
+    println("Shuffled cards:")
+    println(cards)
 }
